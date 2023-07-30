4d1f338b0072f75844df7aec3d9c8c709ea95680
Oleksandr Klants <oleksandr.klants@miquido.com>
2023-07-30 15:21:53 +0200

    update test

--- cypress/e2e/01-Contact-us.js
+++ cypress/e2e/01-Contact-us.js
@@ -3,14 +3,16 @@
 describe('Test contact us form via WebDriverUni', () => {
     beforeEach(() => {
         cy.visit(Cypress.env("baseUrl"));
-        cy.get("#contact-us").invoke("removeAttr", "target").click({force: true});
+        cy.get("#contact-us")
+          .invoke("removeAttr", "target")
+          .click({force: true});
     })
 
     it('Should be able to submit a successful submission via contact us form', () => {
         cy.document().should("have.property", "charset").and("eq", "UTF-8")
-
+        cy.title().should("include", "WebDriver | Contact Us");
+        cy.url().should("contain", "contactus")
         cy.get(".section_header").should("have.text", "CONTACT US");
-
     });
 })
 
