31550ab65253b7872ec7372fc0acb5bdd4525dc8
Oleksandr Klants <oleksandr.klants@miquido.com>
2023-07-31 09:57:44 +0200

    update test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
-    chromeWebSecurity: false,
-    experimentalSessionAndOrigin: true,
-  },
+    env: {
+        "baseUrl": "http://webdriveruniversity.com/"
+    },
+    e2e: {
+        // Configure your E2E tests here
+        specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+        chromeWebSecurity: false,
+        experimentalSessionAndOrigin: true,
+    },
 })
