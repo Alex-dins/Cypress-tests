60c5ec93b8848be3adf8bd98b537f6a37d34faa6
Oleksandr Klants <oleksandr.klants@miquido.com>
2023-08-03 10:24:25 +0200

    add another test "handling js alerts"

--- cypress/e2e/web-driver-uni/03-Iframe.js
+++ cypress/e2e/web-driver-uni/03-Iframe.js
@@ -4,15 +4,22 @@ describe('Handling iframe and modals', () => {
     // Visit page http://webdriveruniversity.com/Popup-Alerts/index.html
     beforeEach(() => {
         cy.visit("/");
-        cy.selectedPage('##iframe');
+        cy.selectedPage('#iframe');
     })
 
     it('Handle iframe and modals', () => {
         cy.get("#frame").then(($iframe) => {
             const body = $iframe.contents().find("body");
             cy.wrap(body).as("iframe");
-        })
-
+        });
+        cy.get("@iframe").find("#button-find-out-more").click();
+        cy.get("@iframe").find(".modal-content").should("be.visible");
+        cy.get("@iframe").find("#myModal").as("modal");
+        cy.get("@modal").should(($expectedText) => {
+            const text = $expectedText.text();
+            expect(text).to.include("Welcome to webdriveruniversity.com we sell a wide range of electrical goods");
+        });
+        cy.get("@modal").find("button").eq(1).click();
+        cy.get("@iframe").find(".modal-content").should("not.be.visible");
     });
-
 });
