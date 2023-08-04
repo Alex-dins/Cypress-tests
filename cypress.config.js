0321f6d1faf2e02ed8b2e5881eedc0e7ea869232
Oleksandr Klants <oleksandr.klants@miquido.com>
2023-08-04 09:10:53 +0200

    change url in config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const {defineConfig} = require('cypress')
 module.exports = defineConfig({
     e2e: {
         // Configure your E2E tests here
-        baseUrl: "http://webdriveruniversity.com/",
+        baseUrl: "http://webdriveruniversity.com",
         specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
         chromeWebSecurity: false,
         experimentalSessionAndOrigin: true,
