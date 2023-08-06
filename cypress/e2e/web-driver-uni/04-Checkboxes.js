5a62a3813a06c4e3249d0d24b2ece95c6f213018
Oleksandr Klants <oleksandr.klants@miquido.com>
2023-08-06 13:53:13 +0200

    update existing test in checkbox file

--- cypress/e2e/web-driver-uni/04-Checkboxes.js
+++ cypress/e2e/web-driver-uni/04-Checkboxes.js
@@ -12,13 +12,20 @@ describe("Handle checkboxes", () => {
         cy.get("input[type='checkbox']")
           .check(["option-1", "option-2", "option-4"])
           .should("be.checked");
-        cy.get("#radio-buttons").find("input").should("have.length", 5)
     })
 
     it("Validate and check radio buttons", () => {
-        cy.get("h1").should("have.text", "Dropdown Menu(s), Checkboxe(s) & Radio Button(s)");
         cy.get("#radio-buttons").find("input").should("have.length", 5);
         cy.get("#radio-buttons").children().last().check().should("be.checked");
+    })
+
+    it("Validate the state of specific radio buttons", () => {
+        cy.getByValue("lettuce").should("not.be.checked");
+        cy.getByValue("cabbage").should("be.disabled");
+        cy.getByValue("pumpkin").should("be.checked");
+        cy.getByValue("lettuce").check();
+        cy.getByValue("lettuce").should("be.checked");
+        cy.getByValue("pumpkin").should("not.be.checked");
     })
 })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,12 @@
 Cypress.Commands.add("getByInput", (selector) => {
     cy.get(`input[${selector}]`);
-    // cy.get(`input[name='${selector}']`);
 });
 Cypress.Commands.add("selectedPage", (selectedPage) => {
     cy.get(selectedPage)
       .invoke("removeAttr", "target")
       .click({force: true});
-})
+});
+
+Cypress.Commands.add("getByValue", (selector) => {
+    cy.get(`[value=${selector}]`);
+});
