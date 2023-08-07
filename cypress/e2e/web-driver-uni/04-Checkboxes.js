590fb7a650b047d281c7d447d8a8f86a97676c85
Oleksandr Klants <oleksandr.klants@miquido.com>
2023-08-07 21:33:10 +0200

    update existing test in checkbox file

--- cypress/e2e/web-driver-uni/04-Checkboxes.js
+++ cypress/e2e/web-driver-uni/04-Checkboxes.js
@@ -16,7 +16,11 @@ describe("Handle checkboxes", () => {
 
     it("Validate and check radio buttons", () => {
         cy.get("#radio-buttons").find("input").should("have.length", 5);
-        cy.get("#radio-buttons").children().last().check().should("be.checked");
+        cy.get("#radio-buttons")
+          .children()
+          .last()
+          .check()
+          .should("be.checked");
     })
 
     it("Validate the state of specific radio buttons", () => {
@@ -26,6 +30,21 @@ describe("Handle checkboxes", () => {
         cy.getByValue("lettuce").check();
         cy.getByValue("lettuce").should("be.checked");
         cy.getByValue("pumpkin").should("not.be.checked");
+    })
+
+    it.only("Validate and check dropdown menu", () => {
+        //Assertion of the first dropdown menu
+        cy.get("#dropdowm-menu-1").contains("JAVA");
+        cy.get("#dropdowm-menu-1")
+          .select("C#").should("have.value", "c#");
+        //Assertion of the second dropdown menu
+        cy.get("#dropdowm-menu-2").contains("Eclipse");
+        cy.get("#dropdowm-menu-2")
+          .select("TestNG").should("have.value", "testng");
+        //Assertion of the third dropdown menu
+        cy.get("#dropdowm-menu-3").contains("HTML");
+        cy.get("#dropdowm-menu-3")
+          .select("JavaScript").should("have.value", "javascript");
     })
 })
 
