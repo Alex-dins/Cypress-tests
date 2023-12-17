32992b83d0e2e3003eb90b47799e237880d8bbdb
Oleksandr Klants <oleksandr.klants@miquido.com>
2023-12-17 18:53:42 +0100

    Refactor accordion tests

--- cypress/e2e/web-driver-uni/08-Accordions.spec.js
+++ cypress/e2e/web-driver-uni/08-Accordions.spec.js
@@ -1,6 +1,7 @@
 /// <reference types = "Cypress" />
 import {HomePage} from "../../support/PageObjects/web-driver-uni/homePage";
 import {AccordionsPage} from "../../support/PageObjects/web-driver-uni/accordionsPage";
+import {AccordionPageContent} from "../../support/PageContents/web-driver-uni/accordionPageContent";
 
 const homePage = new HomePage();
 const accordionsPage = new AccordionsPage()
@@ -11,42 +12,42 @@ describe("Test accordions & text affects", () => {
     })
 
     it("Assert elements on the page", () => {
-        accordionsPage.mainHeader().contains("Click on One of the Accordian Items Below!");
+        accordionsPage.mainHeader().contains(AccordionPageContent.headerText);
         accordionsPage.lengthOfAccordions().should("have.length", 4);
         //Verify state of Loading text
-        accordionsPage.loadingText().should("have.text", "LOADING.. PLEASE WAIT..");
+        accordionsPage.loadingText().should("have.text", AccordionPageContent.loadingTextWait);
         //After 5sec
-        accordionsPage.loadingText(10000).should("have.text", "LOADING COMPLETE.");
+        accordionsPage.loadingText(10000).should("have.text", AccordionPageContent.loadingTextComplete);
     });
 
     it("Should open first Accordion", () => {
-        accordionsPage.firstAccordion().should("have.class", "accordion");
+        accordionsPage.firstAccordion().should("have.class", AccordionPageContent.accordions.first.inactiveClass);
         accordionsPage.firstAccordion().click();
-        accordionsPage.firstAccordionText().contains("Manual testing has for some time");
-        accordionsPage.firstAccordion().should("have.class", "accordion active");
+        accordionsPage.firstAccordionText().contains(AccordionPageContent.accordions.first.text);
+        accordionsPage.firstAccordion().should("have.class", AccordionPageContent.accordions.first.activeClass);
     });
 
     it("Should open second Accordion", () => {
-        accordionsPage.secondAccordion().should("have.class", "accordion");
+        accordionsPage.secondAccordion().should("have.class", AccordionPageContent.accordions.second.inactiveClass);
         accordionsPage.secondAccordion().click();
-        accordionsPage.secondAccordionText().contains("Cucumber (BDD) simplifies the requirement");
-        accordionsPage.secondAccordion().should("have.class", "accordion active");
+        accordionsPage.secondAccordionText().contains(AccordionPageContent.accordions.second.text);
+        accordionsPage.secondAccordion().should("have.class", AccordionPageContent.accordions.second.activeClass);
     });
 
     it("Should open third Accordion", () => {
-        accordionsPage.thirdAccordion().should("have.class", "accordion");
+        accordionsPage.thirdAccordion().should("have.class", AccordionPageContent.accordions.third.inactiveClass);
         accordionsPage.thirdAccordion().click();
-        accordionsPage.thirdAccordionText().contains("Automation testing has been steadily grown");
-        accordionsPage.thirdAccordion().should("have.class", "accordion active");
+        accordionsPage.thirdAccordionText().contains(AccordionPageContent.accordions.third.text);
+        accordionsPage.thirdAccordion().should("have.class", AccordionPageContent.accordions.third.activeClass);
     });
 
     it("Should wait 5sec and open fourth Accordion", () => {
-        accordionsPage.fourthAccordion().should("have.class", "accordion");
+        accordionsPage.fourthAccordion().should("have.class", AccordionPageContent.accordions.fourth.inactiveClass);
         //Wait 5sec and click
         accordionsPage.waitForAccordion(10000)
         accordionsPage.fourthAccordion().click();
-        accordionsPage.fourthAccordionText().contains("This text has appeared after 5 seconds!");
-        accordionsPage.fourthAccordion().should("have.class", "accordion active");
+        accordionsPage.fourthAccordionText().contains(AccordionPageContent.accordions.fourth.text);
+        accordionsPage.fourthAccordion().should("have.class", AccordionPageContent.accordions.fourth.activeClass);
     });
 
 })
---
+++ cypress/support/PageContents/web-driver-uni/accordionPageContent.js
@@ -0,0 +1,27 @@
+export const AccordionPageContent = {
+    headerText: "Click on One of the Accordian Items Below!",
+    loadingTextWait: "LOADING.. PLEASE WAIT..",
+    loadingTextComplete: "LOADING COMPLETE.",
+    accordions: {
+        first: {
+            inactiveClass: "accordion",
+            text: "Manual testing has for some time",
+            activeClass: "accordion active",
+        },
+        second: {
+            inactiveClass: "accordion",
+            text: "Cucumber (BDD) simplifies the requirement",
+            activeClass: "accordion active",
+        },
+        third: {
+            inactiveClass: "accordion",
+            text: "Automation testing has been steadily grown",
+            activeClass: "accordion active",
+        },
+        fourth: {
+            inactiveClass: "accordion",
+            text: "This text has appeared after 5 seconds!",
+            activeClass: "accordion active",
+        }
+    }
+}
